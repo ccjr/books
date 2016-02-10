f591b967960179c9fefd69dc5601c2c793310d07
Cloves Carneiro Jr <cloves.carneiro@livingsocial.com>
2016-02-10 09:54:36 -0500

    More cap tweaks

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,9 @@
-# config valid only for current version of Capistrano
 lock '3.4.0'
 
 set :application, 'books'
 set :repo_url, 'git@github.com:ccjr/books.git'
 set :user, 'ubuntu'
 set :rbenv_ruby, '2.3.0'
+set :linked_dirs, %w{log tmp/pids}
 
 set :puma_bind, 'tcp://0.0.0.0:9292'
-
-namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 set :stage, :production
 
-server '54.84.154.154', user: 'ubuntu', roles: %w{app web db}
+server '52.91.90.129', user: 'ubuntu', roles: %w{app web db}
